4098aaa44ff0060423f8f2b4f578c1f608184ce8
Fernando Cruz <cruzfernandomartin94@gmail.com>
2022-11-06 15:33:10 -0300

    axios installed. Testing api calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  // AXIOS ESTÁ INSTALADO FER, NO TE OLVIDES
+
+  useEffect(() =>{
+    let endPoint = 'https://restcountries.com/v3.1/all'
+    axios.get(endPoint)
+      .then(res=> console.log(res.data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Hola Fer, tenés axios instalado, ponete las pilas gil.
+      </h1>
     </div>
   );
 }
