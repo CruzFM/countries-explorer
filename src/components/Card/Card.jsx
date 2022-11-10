4ccc4b9997e342958ceb34b9edac72f41ec4f985
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-11-09 23:39:51 -0300

    card created

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+
+
+
+export const { Card } = ()=>{
+    return(
+        <div>
+            <h2>Hola Fer, soy Card</h2>
+        </div>
+    )
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 
 export const Navbar = ()=>{
     return(
-        <header className="flex p-3">
+        <header className="flex p-3 h-24 md:h-14">
             <span className="mr-auto">
                 Where in the world?
             </span>
