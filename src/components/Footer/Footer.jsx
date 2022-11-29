891e46517e798afcdf216120956934102af16f69
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-11-28 23:45:12 -0300

    frontendmentor attribution

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,11 @@
 
 export const Footer = ()=>{
     return(
-        <footer className="flex justify-center items-center border-t-2 mt-2 h-28 p-3 bg-white dark:bg-slate-800">
-            <h5 className="text-sm dark:text-gray-300">
+        <footer className="flex flex-col justify-center items-center border-t-2 mt-2 h-28 p-3 bg-white dark:bg-slate-800">
+            <h5 className="text-sm dark:text-gray-300 p-2">
                 Developed by <a href="https://fernandocruz.vercel.app" className="hover:text-cyan-500">Fernando Cruz</a>
             </h5>
+            <h6 className="text-xs dark:text-gray-300">Challenge by <a href="https://frontendmentor.io" className="hover:text-cyan-500">Frontend Mentor</a></h6>
         </footer>
     )
 }
